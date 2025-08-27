ae340ba75d05c1f057cf901240e436a83ab21da8
gkummarifs <gkummarifs@gmail.com>
2025-08-26 23:36:06 -0400

    Code file updates, running successfully

--- main.py
+++ main.py
@@ -21,16 +21,22 @@ bedrock=boto3.client(service_name= "bedrock-runtime")
 
 
 payload = {
-    ...
+    
+    "prompt" : "[INST]" + prompt + "[/INST]", 
+    "max_gen_len": 512,
+    "temperature": 0.3,
+    "top_p": 0.9
+
 }
+
 body = json.dumps(payload)
-model_id="Llama 3.1 70B Instruct"
+model_id="meta.llama3-8b-instruct-v1:0"
 
 #meta.llama2-70b-chat-v1
 
 response=bedrock.invoke_model(
     body=body,
-    model_id=model_id,
+    modelId=model_id,
     accept="application/json",
     contentType="application/json"
     
